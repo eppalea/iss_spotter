6c1f35dda7c136ab7db7b354357f61e09d37cb77
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-12 06:27:18 +0000

    added fetchISSFlyOverTimes function to print out the callbacks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,10 +8,19 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   // console.log("It worked! Returned IP: ", ip);
 // });
 
-fetchCoordsByIP(`206.116.211.111`, (error, latLong) => {
+// fetchCoordsByIP(`206.116.211.111`, (error, latLong) => {
+//   if (error) {
+//     console.log("Sorry, no dice", error);
+//     return;
+//   }
+//   console.log("Good job! You've outputted lat & long: ", latLong);
+// });
+const myCoords = { latitude: 49.3678, longitude: -122.9278 };
+
+fetchISSFlyOverTimes(myCoords, (error, data) => {
   if (error) {
     console.log("Sorry, no dice", error);
     return;
   }
-  console.log("Good job! You've outputted lat & long: ", latLong);
+  console.log("Good job! ISS is overhead during this window: ", data);
 });
