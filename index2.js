4472efde53425ed4ea29163ac67531e0f5456856
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-14 05:18:22 +0000

    changed the function to only call on nextISSTimesForMyLocation. added the printPassTimes variable as well as a catch for the errors

--- index2.js
+++ index2.js
@@ -1,8 +1,22 @@
 const request = require("request");
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(nextISSTimesForMyLocation)
-  .then(body => console.log(body));
+const printPassTimes = function(nextPasses) {
+  for (const pass of nextPasses) {
+    // console.log('the pass is: ', pass);
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    // console.log("the datetime is: ", datetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("Sorry, no dice: ", error.message);
+  });
+
